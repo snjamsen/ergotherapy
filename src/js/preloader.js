b20c61342d37f990a5e78bb476f733f077ac82cf
Jamsen <yiann.sky@gmail.com>
2023-03-20 21:55:33 +0100

    added underline for h2

---
+++ src/js/preloader.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const preloader = document.querySelector('.preloader-wrapper');
+    setTimeout(function() {
+      preloader.style.display = 'none';
+    }, 3000);
+});
