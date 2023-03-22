071ac54475dd79759818ab7b835311a4083da978
Jamsen <yiann.sky@gmail.com>
2023-03-22 22:03:05 +0100

    finished charity and contact section

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Ukrainian Society of Ergotherapists - web-page build in Bootstrap v.5.2
 Project is created with:
 * Vite.js
 * Bootstrap 5.2
-* SASS/SCSS
+* SASS
 
 ## Setup
 To run this project, install it locally using npm:
--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,30 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+// Import Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// Import 
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    //autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
